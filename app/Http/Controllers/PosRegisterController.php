bbad2255f596da6268e0dcc2f3ab254030a062a9
avinash0a <dbaavin@gmail.com>
2025-10-19 19:32:52 +0530

    Fix missing TerminalUser import

--- app/Http/Controllers/PosRegisterController.php
+++ app/Http/Controllers/PosRegisterController.php
@@ -6,6 +6,7 @@ use App\Models\Account;
 use App\Models\Outlet;
 use App\Models\Shift;
 use App\Models\TerminalSession;
+use App\Models\TerminalUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
